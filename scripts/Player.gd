ccb704453634db1c16ecb59e51e00f22b6fbfaae
Soromytko <NikitaSoromytko@gmail.com>
2023-02-17 14:45:36 +0300

    add movement

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,21 +1,35 @@
-extends Node
-
-#onready var camera = get_node("/root/Spatial/CameraController")
+extends KinematicBody
 
 signal move_event(position)
-var valicity = Vector3.DOWN
+
+export var move_speed = 10
+
+var velocity = Vector3.ZERO
+var move_direction = Vector3.ZERO
+
+func get_input() -> Vector3:
+	var input = Vector3.ZERO
+	input.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input.z = Input.get_action_strength("move_back") - Input.get_action_strength("move_forward")
+	return input.normalized() if input.length() > 1 else input
+
+func move(direction : Vector3):
+	velocity.x = direction.x * move_speed
+#	velocity.y = direction.y * 10
+	velocity.z = direction.z * move_speed
+	velocity = move_and_slide(velocity, Vector3.UP)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-#	print(camera)
-	emit_signal("move_event", self.transform.origin)
-#	camera.follow(self)
 	pass
 	
-func _physics_rocess(delta):
-	move_and_slide()
+func _physics_process(delta):
+	var input = get_input()
+	velocity.y -= 1
+	move(input)
+	print(is_on_floor())
+	emit_signal("move_event", self.transform.origin)
+	pass
