14e459239b44b60a0417a6c6b0a4e2256a251d02
Soromytko <NikitaSoromytko@gmail.com>
2023-03-14 10:02:34 +0300

    rename directories

--- Scripts/Picker.gd
+++ Scripts/Picker.gd
@@ -7,10 +7,6 @@ func _on_Area_body_entered(body):
 	if body is Equipment:
 		inventory.add_item(body)
 	elif body is Item:
-		
-		var item = Item.new()
-		print(item.s)
-		
 		get_parent().get_node("Inventory").add_item0(body)
 		body.queue_free()
 	else:
